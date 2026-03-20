187acc36c1ecf89370b3dd5da644d5c590628d79
Daehwa Seo <sdhwa92@gmail.com>
2026-03-21 07:44:00 +1000

    restructuring llm.py

--- llm.py
+++ llm.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
@@ -6,30 +7,25 @@ from langchain_classic.chains import RetrievalQA
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
+load_dotenv()
 
-def get_ai_message(user_question):
 
-    # 데이터베이스 초기화
+def get_retriever():
     embedding = OpenAIEmbeddings(model="text-embedding-3-large")
     index_name = "kor-tax-with-markdown"
     database = PineconeVectorStore.from_existing_index(index_name, embedding)
+    retriever = database.as_retriever(search_kwargs={"k": 2})
+    return retriever
 
-    # 모델 초기화
-    llm = ChatOpenAI(model="gpt-4o")
 
-    # 프롬프트 가져오기
-    client = Client()
-    prompt = client.pull_prompt("rlm/rag-prompt")
-    # 검색 쿼리 처리
-    database.as_retriever(search_kwargs={"k": 2})
+def get_llm(model="gpt-4o"):
+    return ChatOpenAI(model=model)
 
-    # 질문 처리
-    qa_chain = RetrievalQA.from_chain_type(
-        llm, retriever=database.as_retriever(), chain_type_kwargs={"prompt": prompt}
-    )
 
+def get_dictionary_chain():
     # 사전 처리
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    llm = get_llm()
 
     # Define the Prompt Template
     prompt = ChatPromptTemplate.from_messages(
@@ -58,6 +54,27 @@ def get_ai_message(user_question):
 
     # Create the Chain
     dictionary_chain = prompt | llm | output_parser
+
+    return dictionary_chain
+
+
+def get_qa_chain():
+    client = Client()
+    llm = get_llm()
+    retriever = get_retriever()
+    prompt = client.pull_prompt("rlm/rag-prompt")
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm, retriever=retriever, chain_type_kwargs={"prompt": prompt}
+    )
+    return qa_chain
+
+
+def get_ai_message(user_question):
+    # 사전 처리 체인 생성
+    dictionary_chain = get_dictionary_chain()
+
+    # 질문 처리
+    qa_chain = get_qa_chain()
 
     # 체인 생성
     tax_chain = {"query": dictionary_chain} | qa_chain
